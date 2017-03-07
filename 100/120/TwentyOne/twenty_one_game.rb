0d809a7bf65aeec7619529864232b75942caba79
Preston Macy <preston.macy@gmail.com>
2017-03-07 09:20:03 -0600

    refactored twenty_one_game

--- 100/120/TwentyOne/twenty_one_game.rb
+++ 100/120/TwentyOne/twenty_one_game.rb
@@ -1,126 +1,50 @@
-class Game
-  attr_reader :human, :computer, :deck
-
-  def initialize
-    @human = Player.new
-    @computer = Player.new(:dealer)
-    @deck = Deck.new
-    @game_over = false
-  end
-
-  def play
-    welcome
-    deal_cards
-    puts display_all_hands
-    player_hit
-    player_bust? ? display_winner : computer_hit
-    clear
-    puts display_winner
-    puts ""
-    display_all_hands
-    goodbye
-  end
-
-  def deal_cards
-    2.times do
-      human.cards << deck.deck.pop
-      computer.cards << deck.deck.pop
-    end
-  end
-
-  def display_winner
-    @game_over = true
-    puts ""
-    return "Player Bust! You Lost." if player_bust?
-    return "Dealer Bust! You Won!" if computer_bust?
-    if human.hand_amount > computer.hand_amount
-      "You Won!"
+module Hand
+  def display_cards(game_over = false)
+    if type == :player
+      puts cards
     else
-      "Dealer Won!"
+      hidden = [cards.first]
+      (cards.size - 1).times { hidden << "Hidden Card" }
+      puts hidden unless game_over
+      puts cards if game_over
     end
   end
 
-  def player_bust?
-    human.hand_amount > 21
+  def hand_amount
+    num_aces = 0
+    sum = 0
+    cards.each { |c, _| num_aces += 1 if c.split.first == 'Ace' }
+    cards.each { |card| sum += Deck::TOTAL_VALUES[card] }
+    num_aces.times { |_| hand -= 10 if hand > 21 }
+    sum
   end
 
-  def computer_bust?
-    computer.hand_amount > 21
-  end
-
-  def hit?
-    puts ""
-    puts "Hit or Stand? (h/s)"
-    answer = gets.chomp.downcase
-    answer.start_with?('h') ? true : false
-  end
-
-  def player_hit
-    loop do
-      break unless hit?
-      human.cards << deck.deck.pop
-      system "clear"
-      display_all_hands
-      return if human.hand_amount > 21
-    end
-  end
-
-  def display_all_hands
-    puts "===== Human Hand: #{human.hand_amount} ====="
-    display_cards(human)
-    puts ""
-    puts "===== Dealer Hand#{": #{computer.hand_amount}" if @game_over} ====="
-    display_cards(computer)
-  end
-
-  def clear
-    system "clear"
-  end
-
-  def display_cards(player)
-    if player.type == :player
-      puts player.cards
-    else
-      hidden = [player.cards[0]]
-      (player.cards.size - 1).times { hidden << "Hidden Card" }
-      puts hidden unless @game_over
-      puts player.cards if @game_over
-    end
-  end
-
-  def computer_hit
-    loop do
-      break if computer.hand_amount > 17
-      computer.cards << deck.deck.pop
-      clear
-      display_all_hands
-      sleep 1
-    end
-  end
-
-  def welcome
-    clear
-    puts "Welcome to 21 Blackjack."
-  end
-
-  def goodbye
-    puts ""
-    puts "Thank you for playing, good bye."
+  def bust?
+    hand_amount > 21
   end
 end
 
 class Player
-  attr_accessor :cards, :score, :hand_amount, :type
+  include Hand
+
+  attr_accessor :cards, :score, :type, :name
 
   def initialize(type = :player)
     self.type = type
-    self.cards = []
-    self.score = 0
-    self.hand_amount = 0
+    self.name = ''
+    reset
   end
 
-  def hand_amount
-    cards.inject(0) { |sum, card| sum += Deck::TOTAL_VALUES[card] }
+  def player_hit?
+    puts ""
+    puts "Hit or Stand? (h/s)"
+    answer = gets.chomp.downcase
+    answer.start_with?('h')
+  end
+
+  def reset
+    self.cards = []
+    self.score = 0
   end
 end
 
@@ -128,7 +52,7 @@ class Deck
   SUITES = %w(Hearts Spades Diamonds Clubs)
   CARDS = %w(2 3 4 5 6 7 8 9 10 Jack Queen King Ace)
   VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
-  TOTAL_VALUES = SUITES.map.with_object(hsh = Hash.new) do |s, hsh|
+  TOTAL_VALUES = SUITES.each_with_object(hsh = Hash.new) do |s, hsh|
     CARDS.each_with_index do |c, i|
       hsh["#{c} of #{s}"] = VALUES[i]
     end
@@ -152,6 +76,123 @@ class Deck
 
   def card_value(card)
     TOTAL_VALUES[card]
+  end
+end
+
+class Game
+  attr_reader :human, :computer, :deck
+
+  def initialize
+    @human = Player.new
+    @computer = Player.new(:dealer)
+    @deck = Deck.new
+    reset
+  end
+
+  def play
+    welcome
+    loop do
+      deal_cards
+      display_all_hands
+      player_hit
+      human.bust? ? display_winner : computer_hit
+      puts display_winner
+      puts ""
+      display_all_hands
+      break unless play_again?
+      reset
+    end
+    goodbye
+  end
+
+  def reset
+    clear
+    deck.reset
+    human.reset
+    computer.reset
+    @game_over = false
+  end
+
+  def play_again?
+    puts ""
+    puts "Would you like to play again?"
+    gets.chomp.downcase.start_with?('y')
+  end
+
+  def deal_cards
+    2.times do
+      human.cards << deck.deck.pop
+      computer.cards << deck.deck.pop
+    end
+  end
+
+  def player_hit
+    loop do
+      break unless human.player_hit?
+      human.cards << deck.deck.pop
+      system "clear"
+      display_all_hands
+      return if human.bust?
+    end
+  end
+
+  def computer_hit
+    loop do
+      break if computer.hand_amount > 17
+      computer.cards << deck.deck.pop
+      clear
+      display_all_hands
+      sleep 1
+    end
+  end
+
+  def hit
+    if type == :player
+      player_hit
+    else
+      computer_hit
+    end
+  end
+
+  def display_winner
+    @game_over = true
+    clear
+    return "Player Bust! You Lost." if human.bust?
+    return "Dealer Bust! You Won!" if computer.bust?
+    if human.hand_amount > computer.hand_amount
+      "You Won!"
+    else
+      "Dealer Won!"
+    end
+  end
+
+  def display_all_hands
+    puts "===== #{human.name}'s' Hand: #{human.hand_amount} ====="
+    human.display_cards
+    puts ""
+    puts "===== Dealer Hand#{": #{computer.hand_amount}" if @game_over} ====="
+    computer.display_cards(@game_over)
+  end
+
+  def clear
+    system "clear"
+  end
+
+  def welcome
+    clear
+    puts "Welcome to 21 Blackjack."
+    loop do
+      puts "What's your name?"
+      human.name = gets.chomp.capitalize
+      break unless human.name.empty?
+      puts "Sorry, must enter a value"
+    end
+    clear
+  end
+
+  def goodbye
+    clear
+    puts "Thank you for playing, good bye."
   end
 end
 
