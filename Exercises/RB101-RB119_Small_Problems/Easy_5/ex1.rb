79f5a50db1d26b2070637e084995dc3b28b873b5
Preston Macy <preston.macy@gmail.com>
2023-02-23 12:34:29 -0500

    merged launchschool folders - added exercises, book exercises, and organized folders

---
+++ Exercises/RB101-RB119_Small_Problems/Easy_5/ex1.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Write a method that determines and returns the ASCII string value of a string
+# that is passed in as an argument. The ASCII string value is the sum of the
+# ASCII values of every character in the string. (You may use String#ord to
+# determine the ASCII value of a character.)
+
+def ascii_value(str)
+  return 0 if str.empty?
+
+  str.codepoints.map(&:to_i).reduce(&:+)
+end
+
+puts ascii_value('Four score') == 984
+puts ascii_value('Launch School') == 1251
+puts ascii_value('a') == 97
+puts ascii_value('') == 0
